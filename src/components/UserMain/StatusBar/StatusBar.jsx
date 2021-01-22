a75c9d51851901f9282d94f3556b3641879addc9
MJurayev <jurayevmansurbek667@gmail.com>
2021-01-22 17:36:04 +0500

    StatusBar is solved

--- src/components/UserMain/StatusBar/StatusBar.jsx
+++ src/components/UserMain/StatusBar/StatusBar.jsx
@@ -6,13 +6,16 @@ export default function StatusBar(props) {
     const progressStyle={
         width:((props.current-props.start)/(props.end-props.start))*100+'%'
     }
+    const hiddenStyle = {
+        display:props.current === props.end ? 'none' : ''
+    }
     return (
         <div className="coninerStatus">
             <div className="track">
                 <span className="start">{props.start} ёш</span>
                 <span className="end">{props.end} ёш</span>
                 <div style={progressStyle} className="inner">
-                    <span className="current">{props.current} ёш</span>
+                    <span style={hiddenStyle} className="current">{props.current} ёш</span>
                 </div>
             </div>
         </div>
